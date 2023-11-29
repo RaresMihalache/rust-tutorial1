b23cc2e570db5e0d625ba6cebdbe5ae5986282da
Rares <raresmihalache@yahoo.com>
2023-11-29 15:30:38 +0200

    arrays

--- section1/data_types/src/main.rs
+++ section1/data_types/src/main.rs
@@ -43,4 +43,12 @@ fn main() {
     println!("{}", y); // hi
     println!("{}", z); // true
 
+    // Arrays -> fixed length (can not grow in size). It MUST have elements that are the same data-type.
+    let array = [1, 2, 3];
+    println!("{}", array[0]);
+
+    let mut array2: [i32; 3] = [4, 5, 6];
+    println!("{}", array2[0]);
+    array2[0] = 10;
+    println!("{}", array2[0]);
 }
