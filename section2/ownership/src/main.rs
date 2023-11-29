3440f343a0eaa5635e1e9b7606749929e6d6a344
Rares <raresmihalache@yahoo.com>
2023-11-30 00:06:45 +0200

    move

--- section2/ownership/src/main.rs
+++ section2/ownership/src/main.rs
@@ -1,8 +1,14 @@
 fn main() {
-    let var = 1; // var is now valid for the rest of the main function
-    // 1 value is i32 - it has a fixed size -> pushed onto the stack.
-    let mut s = "hello".to_tring(); // created on the heap
-    s.push_str(", world");
+    // let var = 1; // var is now valid for the rest of the main function
+    // // 1 value is i32 - it has a fixed size -> pushed onto the stack.
+    // let mut s = "hello".to_tring(); // created on the heap
+    // s.push_str(", world");
+
+    // Move mechanics
+    let x = vec!["tyler".to_string()];
+    let y = x;
+    println!("{:?}", x); // error: borrow of moved value: `x`
+    println!("{:?}", y);
 }
 // var is dropped - freed from memory
 // s is dropped
