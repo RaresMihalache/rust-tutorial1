d7b5925e11b2e1884af0fe65e16e220039611d35
Rares <raresmihalache@yahoo.com>
2023-11-30 00:17:16 +0200

    Copy mechanism

--- section2/ownership/src/main.rs
+++ section2/ownership/src/main.rs
@@ -4,11 +4,17 @@ fn main() {
     // let mut s = "hello".to_tring(); // created on the heap
     // s.push_str(", world");
 
-    // Move mechanics
+    // Move mechanism
     let x = vec!["tyler".to_string()];
     let y = x.clone(); // solve the below error of ownership by making a clone
     println!("{:?}", x); // error: borrow of moved value: `x`
     println!("{:?}", y);
+
+    // Copy mechanism
+    let x = 1;
+    let y = x;
+    println!("x = {}, y = {}", x, y); // this will work - copy is implemented on types that are stored on the stack. (ints, floats, booleans, chars)
+    // A tuple can also have the copy trait if every element inside it implements copy.
 }
 // var is dropped - freed from memory
 // s is dropped
